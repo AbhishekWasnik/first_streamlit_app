49b6979fbac080577046c3c22c4f6509864850dc
AbhishekWasnik <117827666+AbhishekWasnik@users.noreply.github.com>
2023-10-17 14:12:53 +0530

    Update streamlit_app.py
    Fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header("BREAKFAST")
 streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale,Spinach & Rock Smoothie)
+streamlit.text("Kale,Spinach & Rock Smoothie")
