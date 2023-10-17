c1661c0de6ba5f66cd53398ff026be9aad6dae73
AbhishekWasnik <117827666+AbhishekWasnik@users.noreply.github.com>
2023-10-17 14:12:08 +0530

    Update streamlit_app.py
    Added Breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header("BREAKFAST")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale,Spinach & Rock Smoothie)
